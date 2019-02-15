7f6a424282c7de5d1f4ace0f98a042b1e2418e5e
ktan114 <ktaneight@gmail.com>
2019-02-15 14:36:28 -0500

    Connect the server to the database

--- api/server.js
+++ api/server.js
@@ -1,12 +1,19 @@
-const express = require("express");
-const cors = require("cors");
-const helmet = require("helmet");
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
+const URI = require('./config/keys').URI;
 
+mongoose
+  .connect(URI, { useNewUrlParser: true })
+  .then(() => console.log('Database is connected\n'))
+  .catch(() => console.log('Database is not connected\n'));
 const server = express();
 
 server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
 server.use(cors());
 server.use(helmet());
 
 const port = process.env.PORT || 5000;
-server.listen(port, () => console.log(`Connected on port: ${port}\n`) )
+server.listen(port, () => console.log(`\nServer running on port: ${port}`));
