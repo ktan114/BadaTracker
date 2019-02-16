ac201db70097cb067daa4097150094a9c9f00500
ktan114 <ktaneight@gmail.com>
2019-02-16 13:36:48 -0500

    Set up GET request to retrieve schedules

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 
+const apiPath = require('./config/API.json');
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
+
+  componentDidMount() {
+    this.getAllSchedules();
+  }
+
+  getAllSchedules = () => {
+    axios
+      .get(`${apiPath[apiPath.basePath]}api/schedules`)
+      .then(res => {
+        console.log("Found", res);
+      })
+      .catch(err => console.log('Error'));
+  };
+
   render() {
     return (
       <div className="App">
