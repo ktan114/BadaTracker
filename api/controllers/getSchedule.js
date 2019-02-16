f3c7db62247e9d8dd3e385eaa1117bd6c927af49
ktan114 <ktaneight@gmail.com>
2019-02-15 21:28:27 -0500

    GET route to retrieve a schedule

---
+++ api/controllers/getSchedule.js
@@ -0,0 +1,17 @@
+const Schedule = require('../models/Schedule');
+
+/*
+    @route  GET api/schedule/:id
+    @desc   Retrieves the specific schedule for that date
+    @access Public (Development) | Private (Production)
+*/
+
+const getSchedule = (req, res) => {
+  const { id } = req.params;
+
+  Schedule.findOne({ _id: id })
+    .then(schedule => res.status(200).json({ schedule }))
+    .catch(err => res.status(500).json({ err, message: 'Error' }));
+};
+
+module.exports = { getSchedule };
