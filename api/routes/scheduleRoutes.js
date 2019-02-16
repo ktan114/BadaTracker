91a015b540ec69e5766935e79cd2259843e41cf9
ktan114 <ktaneight@gmail.com>
2019-02-15 22:02:56 -0500

    Configure routes

---
+++ api/routes/scheduleRoutes.js
@@ -0,0 +1,13 @@
+const {
+  getSchedule,
+  getAllSchedule,
+  postSchedule,
+  putSchedule,
+} = require('../controllers');
+
+module.exports = server => {
+  server.route('/api/schedules/:id').get(getSchedule);
+  server.route('/api/schedules').get(getAllSchedule);
+  server.route('/api/schedules/:id').post(postSchedule);
+  server.route('/api/schedules/:id').put(putSchedule);
+};
