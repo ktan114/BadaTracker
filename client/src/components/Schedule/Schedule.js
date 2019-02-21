695ea612bba4b2c85b702e176077a0186a1e31ae
ktan114 <ktaneight@gmail.com>
2019-02-20 22:45:47 -0500

    Placed PropTypes check on schedule and getAlLSchedules prop being sent into the Schedule component

--- client/src/components/Schedule/Schedule.js
+++ client/src/components/Schedule/Schedule.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import DateDisplay from '../DateDisplay/DateDisplay';
+import Medicine from '../Medicine/Medicine';
 
 class Schedule extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            schedule: props.schedule
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      schedule: props.schedule,
+    };
+  }
 
-    render() {
-        const { schedule } = this.state;
-        return (
-            <div>
-                <DateDisplay strDate={ schedule.currentDate } />
-            </div>
-        )
-    }
-
+  render() {
+    const { schedule } = this.state;
+    return (
+      <div>
+        <DateDisplay strDate={schedule.currentDate} />
+        <Medicine medicine={schedule.medicine} />
+      </div>
+    );
+  }
 }
+
+Schedule.propTypes = {
+  schedule: PropTypes.shape({
+    currentDate: PropTypes.string,
+    medicine: PropTypes.bool,
+    trips: PropTypes.number,
+  }).isRequired,
+  getAllSchedules: PropTypes.func.isRequired,
+};
 
 export default Schedule;
