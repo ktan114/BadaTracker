5fd126521fb8112f4d28a4e60bbf4fde524f9696
Kevin Tan <22802321+ktan114@users.noreply.github.com>
2019-02-26 18:54:55 -0500

    Update DeleteSchedule.js
    Attempt to change route to trigger a re-render on the component after delete

--- api/controllers/schedules/deleteSchedule.js
+++ api/controllers/schedules/deleteSchedule.js
@@ -10,7 +10,7 @@ const deleteSchedule = (req, res) => {
   const { id } = req.params;
 
   Schedule.findByIdAndDelete(id)
-    .then(() => res.status(204))
+    .then(() => res.status(200).json({ message: 'Success' })
     .catch(err => res.status(500).json({ err, message: 'Error' }));
 };
 
