2341c22f72a44a359b61217d1d737ecf2049dd69
Kevin Tan <22802321+ktan114@users.noreply.github.com>
2019-02-26 18:27:34 -0500

    Export the delete controller in the index file

--- api/controllers/schedules/index.js
+++ api/controllers/schedules/index.js
@@ -2,10 +2,12 @@ const { getSchedule } = require('./getSchedule');
 const { getAllSchedules } = require('./getAllSchedules');
 const { postSchedule } = require('./postSchedule');
 const { putSchedule } = require('./putSchedule');
+const { deleteSchedule } = require('./deleteSchedule');
 
 module.exports = {
     getSchedule,
     getAllSchedules,
     postSchedule,
-    putSchedule
+    putSchedule,
+    deleteSchedule
 }
