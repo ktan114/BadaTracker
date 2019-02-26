f1af5230b83804e4f385c6660059e05fa5c7064e
ktan114 <ktaneight@gmail.com>
2019-02-25 19:15:15 -0500

    Changed the logic of the click handler

--- client/src/components/Schedule/Schedule.js
+++ client/src/components/Schedule/Schedule.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 import DateDisplay from '../DateDisplay/DateDisplay';
 import Medicine from '../Medicine/Medicine';
@@ -12,10 +13,13 @@ class Schedule extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      schedule: props.schedule,
-      medicine: props.schedule.medicine,
-      trips: props.schedule.trips,
+      schedule: {},
+      id: props.match.params.id,
     };
+  }
+
+  componentDidMount() {
+    this.getSchedule(this.state.id);
   }
 
   handleMedicine = () => {
@@ -32,24 +36,9 @@ class Schedule extends Component {
     });
   };
 
-  handleIncrement = () => {
+  handleClick = value => {
     const stateSchedule = this.state.schedule;
-    stateSchedule.trips++;
-    this.setState((state, props) => ({
-      schedule: stateSchedule,
-      trips: stateSchedule.trips,
-    }));
-    const { medicine, trips } = stateSchedule;
-    this.updateDatabase(stateSchedule._id, {
-      medicine: medicine,
-      trips: trips,
-    });
-  };
-
-  handleDecrement = () => {
-    const stateSchedule = this.state.schedule;
-    if (stateSchedule.trips === 0) stateSchedule.trips = 0;
-    else stateSchedule.trips--;
+    stateSchedule.trips += value;
     this.setState((state, props) => ({
       schedule: stateSchedule,
       trips: stateSchedule.trips,
@@ -68,29 +57,34 @@ class Schedule extends Component {
       .catch(err => console.log('Error', err));
   };
 
+  getSchedule = id => {
+    let schedule = this.state.schedule;
+    axios
+      .get(`${apiPath[apiPath.basePath]}api/schedules/${id}`)
+      .then(res => {
+        schedule = { ...schedule, ...res.data.schedule };
+        this.setState({ schedule: schedule });
+      })
+      .catch(err => console.log('Error', err));
+  };
+
   render() {
-    const { schedule, medicine, trips } = this.state;
+    const { medicine, trips, currentDate } = this.state.schedule;
     return (
       <div>
-        <DateDisplay strDate={schedule.currentDate} />
+        <DateDisplay strDate={currentDate} />
         <Medicine medicine={medicine} handleMedicine={this.handleMedicine} />
-        <Trips
-          trips={trips}
-          handleIncrement={this.handleIncrement}
-          handleDecrement={this.handleDecrement}
-        />
+        <Trips trips={trips} handleClick={this.handleClick} />
+        <button style={{ marginTop: '20px', border: 'none', fontSize: '20px' }}>
+          <Link to="/">Back</Link>
+        </button>
       </div>
     );
   }
 }
 
 Schedule.propTypes = {
-  schedule: PropTypes.shape({
-    currentDate: PropTypes.string,
-    medicine: PropTypes.bool,
-    trips: PropTypes.number,
-  }).isRequired,
-  getAllSchedules: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 export default Schedule;
--- client/src/components/Trips/Trips.js
+++ client/src/components/Trips/Trips.js
@@ -5,16 +5,15 @@ const Trips = props => {
   return (
     <div>
       <h1>Trips: {props.trips}</h1>
-      <button onClick={props.handleIncrement}>Increment</button>
-      <button onClick={props.handleDecrement}>Decrement</button>
+      <button onClick={() => props.handleClick(1)}>Increment</button>
+      <button onClick={() => props.handleClick(-1)}>Decrement</button>
     </div>
   );
 };
 
 Trips.propTypes = {
   trips: PropTypes.number.isRequired,
-  handleIncrement: PropTypes.func.isRequired,
-  handleDecrement: PropTypes.func.isRequired,
+  handleClick: PropTypes.func.isRequired,
 };
 
 export default Trips;
