2c187c40beaf4e87748f590a92b7dfbc8889de37
ktan114 <ktaneight@gmail.com>
2019-02-26 20:08:12 -0500

    Changed the style of the buttons

--- client/src/components/Trips/Trips.js
+++ client/src/components/Trips/Trips.js
@@ -2,11 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Trips = props => {
+
+  const styleButton = {
+    borderRadius: '50%',
+    fontSize: '40px',
+    width: '20%'
+  }
+
   return (
     <div>
       <h1>Trips: {props.trips}</h1>
-      <button onClick={() => props.handleClick(1)}>Increment</button>
-      <button onClick={() => props.handleClick(-1)}>Decrement</button>
+      <div style={{ display: 'flex', justifyContent: 'space-around', width: '50%', marginLeft: '25%' }}>
+        <button style={{...styleButton, color: 'green' }} onClick={() => props.handleClick(1)}>+</button>
+        <button style={{...styleButton, color: 'red'}} onClick={() => props.handleClick(-1)}>-</button>
+      </div>
     </div>
   );
 };
