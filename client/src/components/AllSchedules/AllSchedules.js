dad301538e5982f4066180ecd83bdac71b5c682e
ktan114 <ktaneight@gmail.com>
2019-03-01 00:12:31 -0500

    Added display proptype check and changed the display based on the prop

--- client/src/components/AllSchedules/AllSchedules.js
+++ client/src/components/AllSchedules/AllSchedules.js
@@ -7,16 +7,21 @@ import DeleteSchedule from '../DeleteSchedule/DeleteSchedule';
 import compareDate from '../../helpers/compareDate';
 
 const AllSchedules = props => {
+  const { display, schedules } = props;
   return (
     <div>
-      {props.schedules
-        .slice(0)
+      {schedules
+        .slice(display ? (schedules.length >= 7 ? schedules.length - 7 : 0) : 0)
         .reverse()
         .map(schedule => {
           let color = 'grey';
-          const {_id, currentDate, medicine, trips } = schedule;
+          const { _id, currentDate, medicine, trips } = schedule;
           if (medicine && trips >= 2) color = 'green';
-          if (compareDate(new Date(schedule.currentDate), new Date()) && (!medicine || trips < 2)) color = 'red';
+          if (
+            compareDate(new Date(schedule.currentDate), new Date()) &&
+            (!medicine || trips < 2)
+          )
+            color = 'red';
           return (
             <div
               key={_id}
@@ -42,7 +47,8 @@ const AllSchedules = props => {
 
 AllSchedules.propTypes = {
   schedules: PropTypes.array.isRequired,
-  getAllSchedules : PropTypes.func.isRequired
-}
+  getAllSchedules: PropTypes.func.isRequired,
+  display: PropTypes.bool.isRequired
+};
 
 export default AllSchedules;
