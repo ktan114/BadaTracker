de4b837d664695d85323401aeaaae4f3163e313c
ktan114 <ktaneight@gmail.com>
2019-03-01 18:17:25 -0500

    Imported to project for bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import $ from 'jquery';
+import Popper from 'popper.js';
 
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
